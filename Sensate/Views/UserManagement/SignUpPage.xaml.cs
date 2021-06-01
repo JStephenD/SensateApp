98d8b9782a5900b0607abb362f276eefdbafab1b
alaicadm <45910332+alaicadm@users.noreply.github.com>
2021-06-01 21:30:49 +0800

    small updates on signup
    checking credentials

--- Sensate/Views/UserManagement/SignUpPage.xaml.cs
+++ Sensate/Views/UserManagement/SignUpPage.xaml.cs
@@ -28,19 +28,32 @@ namespace Sensate.Views {
 		}
 		async private void Signup_Clicked(object sender, EventArgs e) {
 			try {
-				//credential checker pa kulang
-				var authProvider = new FirebaseAuthProvider(new FirebaseConfig(FirebaseAPIKey));
-				if (SignUpPassword.Text == SignUpConfirmPassword.Text) {
-					var auth = await authProvider.CreateUserWithEmailAndPasswordAsync(SignUpEmail.Text, SignUpPassword.Text);
-					//string gettoken = auth.FirebaseToken;
-					await this.DisplayAlert("Success!", "Welcome to Sensate.", "OK");
-					await Navigation.PushModalAsync(new AppShell());
+				//credential checker 
+				string email, pword, conf_pword;
+				email = SignUpEmail.Text;
+				pword = SignUpPassword.Text;
+				conf_pword = SignUpConfirmPassword.Text;
 
-				} else {
-					await this.DisplayAlert("Alert", "Password does not match.", "Ok");
+				if (email != null){
+					if(pword != null && conf_pword!= null && pword.Length > 5){
+						if (pword == conf_pword){
+							var auth = await authProvider.CreateUserWithEmailAndPasswordAsync(email, pword);
+							//string gettoken = auth.FirebaseToken;
+							await this.DisplayAlert("Success!", "Welcome to Sensate! Please remember your credentials for future log-ins.", "OK");
+							//redirect to quick profile
+							await Navigation.PushModalAsync(new AppShell());
+						}else{
+							await this.DisplayAlert("Alert", "Password and confirm password don't not match.", "OK");
+						}
+					}else{
+						await this.DisplayAlert("Alert!", "Password must contain atleast 6 characters.", "OK");
+					}
+				}else{
+						await this.DisplayAlert("Alert!", "Email must not be empty.", "OK");
 				}
+
 			} catch (Exception ex) {
-				await this.DisplayAlert("Alert", ex.Message, "Ok");
+				await this.DisplayAlert("Alert", ex.Message, "OK");
 			}
 
 		}
